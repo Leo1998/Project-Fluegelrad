67d38f8ca1b73a8128b416ca25daedcf74a3dcc5
Hinke <HELLO@l.l>
2016-10-11 11:45:46 +0200

    ConnectorTaskParent... nur keine Ahnung ob es workt

--- App/app/src/main/java/de/projectfluegelrad/database/ConnectorTask.java
+++ App/app/src/main/java/de/projectfluegelrad/database/ConnectorTask.java
@@ -11,9 +11,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class ConnectorTask extends AsyncTask<DatabaseAddress, Void, Connection> {
-
-    private Exception exception;
+public class ConnectorTask extends ConnectorTaskParent {
 
     private View view;
 
@@ -23,30 +21,7 @@ public class ConnectorTask extends AsyncTask<DatabaseAddress, Void, Connection> 
 
     @Override
     protected Connection doInBackground(DatabaseAddress... params) {
-        DatabaseAddress address = params[0];
-
-        try {
-            checkConnectivity();
-        } catch(Exception e) {
-            exception = new DatabaseException("No Network!", e);
-            return null;
-        }
-
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            exception = new DatabaseException("Driver missing!", e);
-            return null;
-        }
-
-        try {
-            Connection c = DriverManager.getConnection(address.getUrl(), "testuser", "123456");
-
-            return c;
-        } catch(SQLException e) {
-            exception = new DatabaseException("Failed to connect to Database!", e);
-            return null;
-        }
+        super.doInBackground(DatabaseAddress... params)
     }
 
     @Override
@@ -62,25 +37,4 @@ public class ConnectorTask extends AsyncTask<DatabaseAddress, Void, Connection> 
             }
         }
     }
-
-    private void showMessage(String msg) {
-        Snackbar snackbar = Snackbar.make(view, msg, 5000);
-        snackbar.show();
-    }
-
-    private void checkConnectivity() throws IllegalStateException {
-        ConnectivityManager cm = (ConnectivityManager) view.getContext().getSystemService(Context.CONNECTIVITY_SERVICE);
-
-        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
-        boolean isConnected = activeNetwork != null && activeNetwork.isConnectedOrConnecting();
-
-        if (!isConnected) {
-            throw new IllegalStateException("No Network!");
-        }
-    }
-
-    public Exception getException() {
-        return exception;
-    }
-
 }
