c44bb700875f0e9fc117af70f87916989ef3542c
Leonard <leonard.fricke98@gmail.com>
2016-10-18 16:24:17 +0200

    event saving on internal storage

--- App/app/src/main/java/de/projectfluegelrad/database/DatabaseManager.java
+++ App/app/src/main/java/de/projectfluegelrad/database/DatabaseManager.java
@@ -4,32 +4,47 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.view.View;
 
+import org.json.JSONException;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import de.projectfluegelrad.database.logging.SnackbarLogger;
 
 public class DatabaseManager {
 
-    private View attachedView;
+    private final View attachedView;
+    private final File filesDirectory;
 
     private QueryExecuter queryExecuter;
 
-    public DatabaseManager(View attachedView) {
+    private final List<Event> eventList = new ArrayList<Event>();
+
+    public DatabaseManager(View attachedView, File filesDirectory) {
         this.attachedView = attachedView;
+        this.filesDirectory = filesDirectory;
+        if (!filesDirectory.exists()) {
+            filesDirectory.mkdirs();
+        }
 
         ConnectivityManager cm = (ConnectivityManager) attachedView.getContext().getSystemService(Context.CONNECTIVITY_SERVICE);
 
         this.queryExecuter = new QueryExecuter(new SnackbarLogger(attachedView), cm, new DatabaseAddress("pipigift.ddns.net", 3306, "fluegelrad"), "testuser", "123456");
 
         if (this.queryExecuter.connectAndWait()) {
-            loadEventData();
+            refreshEventData();
+            saveEvents();
         }
     }
 
-    public void loadEventData() {
+    private void refreshEventData() {
         ResultSet result = this.queryExecuter.executeQuery(new BasicQuery("SELECT * FROM events;"));
 
         try {
@@ -48,19 +63,34 @@ public class DatabaseManager {
                 Date date = result.getDate("date");
                 String description = result.getString("description");
 
-                /*StringBuilder builder = new StringBuilder();
-                builder.append(id);
-                builder.append(location);
-                builder.append(category);
-                builder.append(price);
-                builder.append(host);
-                builder.append(date);
-                builder.append(description);
-                System.out.println(builder.toString());*/
+                Event event = new Event(id, location, category, price, host, date, description);
+
+                if (!eventList.contains(event)) {
+                    eventList.add(event);
+                } else {
+                    // already exists...
+                }
             }
         } catch(Exception e) {
             e.printStackTrace();
         }
     }
 
+    private void saveEvents() {
+        for (int i = 0; i < eventList.size(); i++) {
+            Event event = eventList.get(i);
+
+            try {
+                Event.writeEvent(new FileOutputStream(new File(filesDirectory, event.getId() + ".event")), event);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public List<Event> getEventList() {
+        return eventList;
+    }
 }
