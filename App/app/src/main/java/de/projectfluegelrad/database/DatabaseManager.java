d4a81cb516f8bf5fbddf350becc5aff756815681
Leonard <leonard.fricke98@gmail.com>
2016-10-18 15:40:07 +0200

    mvc differenzierung und entpacken der daten

--- App/app/src/main/java/de/projectfluegelrad/database/DatabaseManager.java
+++ App/app/src/main/java/de/projectfluegelrad/database/DatabaseManager.java
@@ -1,10 +1,15 @@
 package de.projectfluegelrad.database;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
 import android.view.View;
 
+import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+
+import de.projectfluegelrad.database.logging.SnackbarLogger;
 
 public class DatabaseManager {
 
@@ -15,29 +20,46 @@ public class DatabaseManager {
     public DatabaseManager(View attachedView) {
         this.attachedView = attachedView;
 
-        this.queryExecuter = new QueryExecuter(attachedView, new DatabaseAddress("pipigift.ddns.net", 3306, "fluegelrad"), "testuser", "123456");
+        ConnectivityManager cm = (ConnectivityManager) attachedView.getContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        this.queryExecuter = new QueryExecuter(new SnackbarLogger(attachedView), cm, new DatabaseAddress("pipigift.ddns.net", 3306, "fluegelrad"), "testuser", "123456");
 
         if (this.queryExecuter.connectAndWait()) {
-            ResultSet result = this.queryExecuter.executeQuery(new BasicQuery("SELECT * FROM events;"));
-            dumpResultSet(result);
+            loadEventData();
         }
     }
 
-    private void dumpResultSet(ResultSet resultSet) {
+    public void loadEventData() {
+        ResultSet result = this.queryExecuter.executeQuery(new BasicQuery("SELECT * FROM events;"));
+
         try {
-            ResultSetMetaData metaData = resultSet.getMetaData();
-            int columnsNumber = metaData.getColumnCount();
+            ResultSetMetaData metaData = result.getMetaData();
 
-            while (resultSet.next()) {
-                for (int i = 1; i <= columnsNumber; i++) {
-                    if (i > 1) System.out.print(",  ");
-                    String columnValue = resultSet.getString(i);
-                    System.out.print(columnValue + " " + metaData.getColumnName(i));
+            while (result.next()) {
+                if (metaData.getColumnCount() != 7) {
+                    throw new DatabaseException("Bad Event Data! (column count == " + metaData.getColumnCount() + ")");
                 }
-                System.out.println("");
-            }
-        } catch(SQLException e) {
 
+                int id = result.getInt("id");
+                String location = result.getString("location");
+                String category = result.getString("category");
+                int price = result.getInt("price");
+                String host = result.getString("host");
+                Date date = result.getDate("date");
+                String description = result.getString("description");
+
+                /*StringBuilder builder = new StringBuilder();
+                builder.append(id);
+                builder.append(location);
+                builder.append(category);
+                builder.append(price);
+                builder.append(host);
+                builder.append(date);
+                builder.append(description);
+                System.out.println(builder.toString());*/
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
         }
     }
 
