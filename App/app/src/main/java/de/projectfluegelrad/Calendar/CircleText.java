d52db4666fb1fc38ece7616124e0d31294014471
DK99 <kaleb.dk@gmail.com>
2016-10-19 11:37:21 +0200

    Circular Text Views finished

--- App/app/src/main/java/de/projectfluegelrad/Calendar/CircleText.java
+++ App/app/src/main/java/de/projectfluegelrad/Calendar/CircleText.java
@@ -12,7 +12,10 @@ import android.graphics.Rect;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.DrawableContainer;
+import android.graphics.drawable.GradientDrawable;
+import android.graphics.drawable.LayerDrawable;
 import android.graphics.drawable.StateListDrawable;
+import android.graphics.drawable.shapes.Shape;
 import android.os.Build;
 import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
@@ -26,21 +29,41 @@ import de.projectfluegelrad.R;
 public class CircleText extends TextView{
     private Drawable drawable;
 
+    private GradientDrawable pressed;
+    private GradientDrawable unpressed;
+
     public CircleText(Context context) {
         super(context);
-        drawable = ContextCompat.getDrawable(context, R.drawable.touch_selector);
+        init();
     }
 
     public CircleText(Context context, AttributeSet attrs) {
         super(context, attrs);
-        drawable = ContextCompat.getDrawable(context, R.drawable.touch_selector);
+        init();
     }
 
     public CircleText(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        drawable = ContextCompat.getDrawable(context, R.drawable.touch_selector);
+        init();
     }
 
+    private void init(){
+        drawable = ContextCompat.getDrawable(getContext(), R.drawable.touch_selector);
+        if (drawable instanceof StateListDrawable){
+            drawable = ContextCompat.getDrawable(getContext(), R.drawable.touch_selector);
+            DrawableContainer.DrawableContainerState d = ((DrawableContainer.DrawableContainerState) drawable.getConstantState());
+            Drawable[] children = d.getChildren();
+
+            Drawable selectedItem = children[0];
+            Drawable unselectedItem = children[1];
+
+            pressed = (GradientDrawable) selectedItem;
+            unpressed = (GradientDrawable) unselectedItem;
+
+            pressed.setColor(ContextCompat.getColor(getContext(), R.color.shadow));
+            unpressed.setColor(Color.TRANSPARENT);
+        }
+    }
 
     @Override
     protected void onDraw(Canvas canvas) {
@@ -58,12 +81,11 @@ public class CircleText extends TextView{
         }
 
         super.drawableStateChanged();
-
     }
 
 
     @Override
     public void setBackgroundColor(int color) {
-        drawable = ContextCompat.getDrawable(getContext(), R.drawable.touch_selector_event);
+        unpressed.setColor(color);
     }
 }
