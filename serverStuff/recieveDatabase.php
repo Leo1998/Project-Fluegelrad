d11df1a8bafcc936aa63b222c11d304489d5a258
Leonard <leonard.fricke98@gmail.com>
2016-12-31 12:03:29 +0100

    recieveDatabase change

--- serverStuff/recieveDatabase.php
+++ serverStuff/recieveDatabase.php
@@ -8,9 +8,9 @@
 	//SQL Statement
 	$eventsGet = $pdo->prepare("SELECT 
 		`events`.`id` , `events`.`name` , `events`.`price` , `events`.`maxParticipants` , `events`.`participants` , `events`.`dateStart` , `events`.`dateEnd` , `events`.`description` , `events`.`ageMin` , `events`.`ageMax` ,`events`.`formId` ,  
-		`locations`.`address`,`locations`.`longitude`,`locations`.`latitude` ,
-		`sponsors`.`mail` , `sponsors`.`phone` , `sponsors`.`web`,
-		`sponsors`.`name` , `sponsors`.`description` , `sponsors`.`image` , `sponsors`.`id` 
+		`locations`.`address` AS `location.address`,`locations`.`longitude` AS `location.longitude`,`locations`.`latitude` AS `location.latitude` ,
+		`sponsors`.`mail` AS host.mail` , `sponsors`.`phone` AS `host.phone` , `sponsors`.`web` AS `host.web`,
+		`sponsors`.`name` AS `host.name` , `sponsors`.`description` AS `host.description` , `sponsors`.`image` AS `host.image` , `sponsors`.`id` AS `host.id` 
 		FROM `events` 
 		JOIN `locations` ON `events`.`locationId` = `locations`.`id`
 		JOIN `hosts` ON `hosts`.`id` = `events`.`hostId`
