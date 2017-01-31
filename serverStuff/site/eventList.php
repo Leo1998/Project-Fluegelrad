5501e29426509072ee286622bb35557ac91d30af
Daniel <hi@swag.ultimateRIPMasters.onion>
2017-01-31 19:09:04 +0100

    EventList fix

--- serverStuff/site/eventList.php
+++ serverStuff/site/eventList.php
@@ -17,15 +17,19 @@
 		JOIN `locations` ON `events`.`locationId` = `locations`.`id`
 		JOIN `hosts` ON `events`.`hostId` = `hosts`.`id`
 		JOIN `sponsors` ON `sponsors`.`id` = `hosts`.`sponsorId`
-		JOIN (SELECT * from `imagePaths` GROUP BY `imagePaths`.`eventId`) AS `imagePaths` ON `imagePaths`.`eventId` = `events`.`id`
+		LEFT JOIN (SELECT * from `imagePaths` GROUP BY `imagePaths`.`eventId`) AS `imagePaths` ON `imagePaths`.`eventId` = `events`.`id`
 		LEFT JOIN (SELECT AVG(`rating`) AS `rating`,`eventId` FROM `eventRatings` GROUP BY `eventId`) AS `ratings` ON `ratings`.`eventId` = `events`.`id`
 		ORDER BY `events`.`dateStart` , `events`.`dateEnd`");
 	$statement->execute();
 	
 	while($row = $statement->fetch(PDO::FETCH_ASSOC)) {
-		$desc = split("\n",$row["description"]);
+		$desc = explode("\n",$row["description"]);
 		unset($row["description"]);
-		$row["description1"] = $desc[0];
+		if(isset($desc[0])){
+			$row["description1"] = $desc[0];
+		}else{
+			$row["description1"] = "";
+		}
 		if(isset($desc[1])){
 			$row["description2"] = $desc[1];
 		}else{
@@ -113,11 +117,11 @@
 			li.appendChild(name);
 			li.appendChild(rating);
 			li.appendChild(img);
+			li.appendChild(sponsorImg);
 			li.appendChild(price);
 			li.appendChild(participants);
 			li.appendChild(dateLoc);
 			li.appendChild(desc);
-			li.appendChild(sponsorImg);
 			
 			document.getElementById('events').appendChild(li);
 			
